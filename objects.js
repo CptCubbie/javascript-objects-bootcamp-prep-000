51fbc39d74ce4f2bcc093668d592fb7a76c75409
Brittany Tinnin <bntinnin7@gmail.com>
2018-03-06 17:46:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,3 @@
-var playlist = { suicidesquad: "purple lamborghini" };
-function updatePlaylist(obj, key, value) {
-  obj[key] = value;
-  return obj;
-}
-
-function removeFromPlaylist(obj, key) {
- delete obj[key];
- return obj;
-}
+var playlist = {
+  "suicidesquad: purple lamborghini"
+};
